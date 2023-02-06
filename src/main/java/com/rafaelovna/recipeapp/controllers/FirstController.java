365ab1c4deaa5c6d63687d52ba0b73a6718e14f1
rafaelovna <aikanush777@mail.ru>
2023-02-06 17:27:00 +0400

    info 2

---
+++ src/main/java/com/rafaelovna/recipeapp/controllers/FirstController.java
@@ -0,0 +1,24 @@
+package com.rafaelovna.recipeapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "<h1 style=\"text-align:center\">Приложение запущено</h1>";
+    }
+
+    @GetMapping("/info")
+    public String printInfo() {
+        return """
+                Ученик: Арутюнян Айкануш Рафаеловна. </br>
+                Проект: Приложение рецептов. Recipe app. </br>
+                Дата создания проекта: 06.02.2023. </br>
+                Описание проекта: Простое и удобное приложение с большим количеством пошаговых рецептов.\s
+                Готовить можно по инструкции с пошаговыми рекомендациями. Приложение помогает учиться готовить, совершенствовать отработанные навыки.""";
+    }
+
+}
