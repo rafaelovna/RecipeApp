8928eb31e90eb588c005edaf9eb5170d8296f1fb
rafaelovna <aikanush777@mail.ru>
2023-02-06 18:21:31 +0400

    info 2

--- src/main/java/com/rafaelovna/recipeapp/controller/FirstController.java
+++ src/main/java/com/rafaelovna/recipeapp/controller/FirstController.java
@@ -1,4 +1,4 @@
-package com.rafaelovna.recipeapp.controllers;
+package com.rafaelovna.recipeapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
