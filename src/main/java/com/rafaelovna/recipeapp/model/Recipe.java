7967d73c7b06fbd6a813413a5692b2d69b5da4e8
rafaelovna <aikanush777@mail.ru>
2023-02-08 17:19:56 +0400

    Recipe

--- src/main/java/com/rafaelovna/recipeapp/model/Recipe.java
+++ src/main/java/com/rafaelovna/recipeapp/model/Recipe.java
@@ -3,9 +3,7 @@ package com.rafaelovna.recipeapp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-
 import java.util.ArrayList;
-
 
 @Data
 @AllArgsConstructor
@@ -13,9 +11,7 @@ public class Recipe {
 
     private String name;
     private int cookingTimeInMin;
-    private Ingredient ingredients;
+    private ArrayList<Ingredient> ingredients;
     private ArrayList<String> steps;
-
-
 
 }
